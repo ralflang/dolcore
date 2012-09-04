e0ccb5c5978ffd95f100f259da35745d8635da91
Ralf Lang <ralf.lang@ralf-lang.de>
2012-09-04 16:35:14 +0200

    Use Horde_Routes to parse the path

--- app/controllers/Feed.php
+++ app/controllers/Feed.php
@@ -3,40 +3,43 @@ class Dolcore_Feed_Controller extends Horde_Controller_Base
 {
     public function processRequest(Horde_Controller_Request $request, Horde_Controller_Response $response)
     {
+        $this->_mapper = $GLOBALS['injector']->getInstance('Horde_Routes_Mapper');
+        $this->_matchDict = new Horde_Support_Array($this->_mapper->match($request->getPath()));
+
         $injector = $this->getInjector();
         $parts = explode('/',$request->getPath());
-        if (count($parts) > 2) {
-            switch ($parts[3]) {
-                case 'category':
-                    $driver = $injector->getInstance('Dolcore_Factory_Driver')->create($injector);
-                    $categories = $driver->getCategoriesApi();
-                    $discussionApi = $driver->getDiscussionApi();
-                    $category = $categories->getCategory($parts[4]);
-                    /* Write a category's currently running Umfragen */
-                    $now = new Horde_Date(time());
 
-                    $template = $this->getInjector()->createInstance('Horde_Template');
+        switch ($this->_matchDict->action) {
+            case 'category':
+                $driver = $injector->getInstance('Dolcore_Factory_Driver')->create($injector);
+                $categories = $driver->getCategoriesApi();
+                $discussionApi = $driver->getDiscussionApi();
+                $category = $categories->getCategory($parts[4]);
+                /* Write a category's currently running Umfragen */
+                $now = new Horde_Date(time());
 
-                    $template->set('updated', $now->format(DATE_ATOM));
-                    $template->set('category_caption', $category->getCaption());
-                    $template->set('category_id', $parts[4]);
+                $template = $this->getInjector()->createInstance('Horde_Template');
 
-                    $discussions = array();
+                $template->set('updated', $now->format(DATE_ATOM));
+                $template->set('category_caption', $category->getCaption());
+                $template->set('category_id', $parts[4]);
 
-                    foreach ($discussionApi->listDiscussions(array('category' => $category->id)) as $discussion) {
-                        $discussions[$discussion->id]['title'] = $discussion->text;
-                        $discussions[$discussion->id]['details'] = $discussion->hintergrund;
-                        $discussions[$discussion->id]['modified'] = $discussion->erstelldatum;
-                    }
-                    $template->set('discussions', $discussions);
+                $discussions = array();
 
-                    $response->setBody($template->fetch(DOLCORE_TEMPLATES . '/feeds/atom.xml'));
-                break;
+                foreach ($discussionApi->listDiscussions(array('category' => $category->id)) as $discussion) {
+                    $discussions[$discussion->id]['title'] = $discussion->text;
+                    $discussions[$discussion->id]['details'] = $discussion->hintergrund;
+                    $discussions[$discussion->id]['modified'] = $discussion->erstelldatum;
+                }
+                $template->set('discussions', $discussions);
 
-                case 'categories':
+                $response->setBody($template->fetch(DOLCORE_TEMPLATES . '/feeds/atom.xml'));
+            break;
 
-                break;
-            }
+            case 'categories':
+
+            break;
         }
+
     }
 }
